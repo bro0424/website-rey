c04d07cb0ebe019af6e76f173cd7e6a935e2227c
bro0424 <akuncockaribo@gmail.com>
2024-08-24 06:46:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+alert('🙋‍♂️')
+alert('HAY Sygkuhhh, Lagi Gabut, Baru Belajar Jdi Cmn Bisa Begini, hehe. Bagi PAP Yaa sygkuhh ')
+
+alert('LOVYOUUUUUU                                                                                                                                                                                                                                                                             jng centang kse oke sja')
+
+prompt('bls apa hayoooo??')
+alert('💖💕😁')
+
+document.getElementById('revealButton').addEventListener('click', function() {
+  document.getElementById('heartContainer').classList.toggle('hidden');
+});
